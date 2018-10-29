f3e93908b4d7d519be0a287c94cfbc4ef377bbf2
software.contreras@gmail.com <software.contreras@gmail.com>
2018-10-29 17:24:14 -0500

    changes on home

--- application/views/footer.php
+++ application/views/footer.php
@@ -8,26 +8,33 @@
                 <div class="bt_bb_column col-md-3 col-sm-6 col-ms-12 bt_bb_align_left bt_bb_vertical_align_top bt_bb_animation_fade_in animate bt_bb_padding_normal"
                   data-width="3">
                   <div class="bt_bb_column_content">
-                    <div class="bt_bb_image bt_bb_shape_square bt_bb_align_inherit bt_bb_hover_style_simple bt_bb_content_display_always bt_bb_content_align_middle"><span><img src="http://oxigeno.bold-themes.com/basketball/wp-content/uploads/sites/6/2017/12/logo_footer.png" alt="http://oxigeno.bold-themes.com/basketball/wp-content/uploads/sites/6/2017/12/logo_footer.png"></span></div>
-                    <div
-                      class="bt_bb_separator bt_bb_bottom_spacing_small bt_bb_border_style_none"></div>
-                  <header class="bt_bb_headline bt_bb_font_weight_normal bt_bb_color_scheme_5 bt_bb_dash_none bt_bb_size_small bt_bb_subheadline bt_bb_align_inherit">
-                    <h3><span class="bt_bb_headline_content"><span>MEET <strong>OXYGEN</strong></span></span>
-                    </h3>
-                    <div class="bt_bb_headline_subheadline">We are a competitive sport club &#8211; with our professional players competing at all levels from local team galas.</div>
-                  </header>
+                    <div class="bt_bb_image bt_bb_shape_square bt_bb_align_inherit bt_bb_hover_style_simple bt_bb_content_display_always bt_bb_content_align_middle">
+                        <span>
+                            <img src="<?php echo site_url().'static/page_front/images/logo/logo_favi.png';?>" alt="<?php echo site_url().'static/page_front/images/logo/logo_favi.png';?>" width="65">
+                        </span>
+                    </div>
+                    <div class="bt_bb_separator bt_bb_bottom_spacing_small bt_bb_border_style_none"></div>
+                    <header class="bt_bb_headline bt_bb_font_weight_normal bt_bb_color_scheme_5 bt_bb_dash_none bt_bb_size_small bt_bb_subheadline bt_bb_align_inherit">
+                      <h3>
+                          <span class="bt_bb_headline_content"><span>LIGA <strong>DE SURCO</strong></span></span>
+                      </h3>
+                      <div class="bt_bb_headline_subheadline">La liga de surco proporciona a los jugadores la oportunidad de aprender y desarrollar habilidades de baloncesto.</div>
+                    </header>
                   <div class="bt_bb_separator bt_bb_bottom_spacing_normal bt_bb_border_style_none"></div>
                   <div class="bt_bb_icon bt_bb_color_scheme_6 bt_bb_style_filled bt_bb_size_normal bt_bb_shape_circle bt_bb_align_inherit">
-                    <a href="https://www.facebook.com/boldthemes/" target="_blank" data-ico-fontawesome="&#xf09a;" class="bt_bb_icon_holder"></a>
+                    <a href="#" target="_blank">
+                        <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
+                    </a>
                   </div>
                   <div class="bt_bb_icon bt_bb_color_scheme_6 bt_bb_style_filled bt_bb_size_normal bt_bb_shape_circle bt_bb_align_inherit">
-                    <a href="https://twitter.com/bold_themes" target="_blank" data-ico-fontawesome="&#xf099;" class="bt_bb_icon_holder"></a>
+                    <a href="#" target="_blank">
+                        <i class="fa fa-instagram fa-2x" aria-hidden="true"></i>
+                    </a>
                   </div>
                   <div class="bt_bb_icon bt_bb_color_scheme_6 bt_bb_style_filled bt_bb_size_normal bt_bb_shape_circle bt_bb_align_inherit">
-                    <a href="#" target="_blank" data-ico-fontawesome="&#xf16d;" class="bt_bb_icon_holder"></a>
-                  </div>
-                  <div class="bt_bb_icon bt_bb_color_scheme_6 bt_bb_style_filled bt_bb_size_normal bt_bb_shape_circle bt_bb_align_inherit">
-                    <a href="https://plus.google.com/106260443376081681677" target="_blank" data-ico-fontawesome="&#xf0d5;" class="bt_bb_icon_holder"></a>
+                      <a href="#" target="_blank">
+                          <i class="fa fa-youtube-play fa-2x" aria-hidden="true"></i>
+                      </a>
                   </div>
                   <div class="bt_bb_separator bt_bb_bottom_spacing_normal bt_bb_border_style_none"></div>
                 </div>
@@ -55,11 +62,10 @@
                       </div>
                       <div class="bt_bb_twitter_item">
                         <div class="content">
-                          <a href="https://twitter.com/bold_themes" target="_blank" title="@bold_themes"><img src="https://pbs.twimg.com/profile_images/1009015506928533504/hykdbRdO_bigger.jpg" class="bt_bb_twitter_avatar" alt="@bold_themes"
-                              title="@bold_themes" /></a><small class="bt_bb_twitter_username"><a href="https://twitter.com/bold_themes/status/1017339986411212801" target="_blank" title="@bold_themes">@bold_themes &#8211; 3 months</a></small>
-                          <p
-                            class="bt_bb_twitter_content">WordPress 4.9.7 is available. If you have updated WP, you will notice that everything work fine with our themes. If… <a href="https://t.co/SXIfrsOPiJ"
-                              class="twitter-link">https://t.co/SXIfrsOPiJ</a></p>
+                          <a href="https://twitter.com/bold_themes" target="_blank" title="@bold_themes">
+                              <img src="https://pbs.twimg.com/profile_images/1009015506928533504/hykdbRdO_bigger.jpg" class="bt_bb_twitter_avatar" alt="@bold_themes"  title="@bold_themes" />
+                          </a>
+                          <small class="bt_bb_twitter_username"><a href="https://twitter.com/bold_themes/status/1017339986411212801" target="_blank" title="@bold_themes">@bold_themes &#8211; 3 months</a></small>
                         </div>
                       </div>
                     </div>
@@ -71,7 +77,7 @@
                 data-width="3">
                 <div class="bt_bb_column_content">
                   <header class="bt_bb_headline bt_bb_font_weight_normal bt_bb_color_scheme_5 bt_bb_dash_none bt_bb_size_small bt_bb_align_inherit">
-                    <h3><span class="bt_bb_headline_content"><span><strong>INSTAGRAM</strong></span></span>
+                    <h3><span class="bt_bb_headline_content"><span><strong>BASES</strong></span></span>
                     </h3>
                   </header>
                   <div class="bt_bb_separator bt_bb_bottom_spacing_normal bt_bb_border_style_none"></div>
--- static/backoffice/js/support.js
+++
@@ -1,7 +0,0 @@
-function show(){
-    $("#form-support").css("display", "block");
-}
-function hide(){
-    $("#form-support").css("display", "none");
-}
-
